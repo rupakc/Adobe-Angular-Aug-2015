1d32dd8d206c6f8529fd8bde497df168fe9391c2
tkmagesh <tkmagesh77@gmail.com>
2015-08-03 12:47:24 +0530

    Generalizing - Part - 2

---
+++ Day-01/02-ModelViewScope/app.js
@@ -0,0 +1,42 @@
+var app = (function(){
+        return {
+            init : function(scope, callback){
+                $("*[app-model]").each(function(index, element){
+                    var $element = $(element),
+                        model = $element.attr("app-model");
+                    $element.change(function(){
+                        var self = this;
+                        scope.$apply(function(){
+                            var expr = model + '=' + self.value;
+                            scope.$eval(expr);
+                        });
+                    });
+                    scope.$watch(model, function(newValue, oldValue){
+                        $element.val( newValue);
+                    })
+                });
+
+
+                $("*[app-click]").each(function(index, element){
+                    var $element = $(element),
+                        expr = $element.attr("app-click");
+
+                    $element.click(function(){
+                        scope.$apply(function(){
+                            scope.$eval(expr);
+                        });
+                    });
+                });
+
+                $("*[app-bind]").each(function(index, element){
+                    var $element = $(element),
+                        model = $element.attr("app-bind");
+                    scope.$watch(model, function(newValue, oldValue){
+                        $element.html(newValue);
+                    });
+                });
+
+                callback();
+            }
+        }
+    })()
